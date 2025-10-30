1a4e1207b4cdb003addd9648690dd2a133de6667
luoluoluo <360646101@qq.com>
2025-10-30 10:58:13 +0800

    tabbar 样式

--- uni-running/components/tabBar.vue
+++ uni-running/components/tabBar.vue
@@ -1,90 +1,99 @@
 <template>
-	<view class="tabbar-box">
-		<u-tabbar :value="active" @change="onChange" activeColor="#FF8C00" inactiveColor="#000" fixed placeholder
-			zIndex="100" safeAreaInsetBottom :border="false">
-			<u-tabbar-item :text="item.text" v-for="(item, index) in tabList" :key="index" :name="item.name">
-				<template #active-icon>
-					<view class="iconfont active" :class="item.icon"></view>
-				</template>
-				<template #inactive-icon>
-					<view class="iconfont" :class="item.icon"></view>
-				</template>
-			</u-tabbar-item>
-		</u-tabbar>
-	</view>
+  <view class="tabbar-box">
+    <u-tabbar
+      :value="active"
+      @change="onChange"
+      activeColor="#FF8C00"
+      inactiveColor="#000"
+      fixed
+      placeholder
+      zIndex="100"
+      safeAreaInsetBottom
+      :border="false"
+    >
+      <u-tabbar-item
+        :text="item.text"
+        v-for="(item, index) in tabList"
+        :key="index"
+        :name="item.name"
+      >
+        <template #active-icon>
+          <view class="iconfont active" :class="item.icon"></view>
+        </template>
+        <template #inactive-icon>
+          <view class="iconfont" :class="item.icon"></view>
+        </template>
+        <template #text>
+          <text class="tabbar-text">{{ item.text }}</text>
+        </template>
+      </u-tabbar-item>
+    </u-tabbar>
+  </view>
 </template>
 <script setup>
-import { ref } from 'vue'
-import { isRelease } from "@/utils/config.js"
+import { ref } from "vue";
+import { isRelease } from "@/utils/config.js";
 
 // Props
-const props = defineProps(['type'])
+const props = defineProps(["type"]);
 
 // 响应式数据
-const active = ref(props.type)
+const active = ref(props.type);
 const tabList = ref([
-	{
-		"text": "首页",
-		name: 'index',
-		"pagePath": "/pages/index",
-		icon: "icon-shouye",
-		normal: '../static/tab_00.png',
-		active: '../static/tab_01.png',
-	},
-	{
-		"text": "运动",
-		name: 'sport',
-		"pagePath": "/pages/sport",
-		icon: "icon-huabi-",
-		normal: '../static/tab_00.png',
-		active: '../static/tab_01.png',
-	},
-	{
-		"text": "我的",
-		name: 'mine',
-		icon: "icon-wenzi",
-		"pagePath": "/pages/mine",
-		normal: '../static/tab_20.png',
-		active: '../static/tab_21.png',
-	},
-])
+  {
+    text: "首页",
+    name: "index",
+    pagePath: "/pages/index",
+    icon: "icon-shouye",
+    normal: "../static/tab_00.png",
+    active: "../static/tab_01.png",
+  },
+  {
+    text: "运动",
+    name: "sport",
+    pagePath: "/pages/sport",
+    icon: "icon-huabi-",
+    normal: "../static/tab_00.png",
+    active: "../static/tab_01.png",
+  },
+  {
+    text: "我的",
+    name: "mine",
+    icon: "icon-wenzi",
+    pagePath: "/pages/mine",
+    normal: "../static/tab_20.png",
+    active: "../static/tab_21.png",
+  },
+]);
 
 // 方法定义
 const onChange = (detail) => {
-	if (detail === props.type) return
+  if (detail === props.type) return;
 
-	uni.switchTab({
-		url: "/pages/" + detail
-	})
-}
+  uni.switchTab({
+    url: "/pages/" + detail,
+  });
+};
 </script>
 
 <style lang="scss" scoped>
-	.tabbar-box {
-		height: 224rpx;
-		box-sizing: content-box;
-		margin-bottom: env(safe-area-inset-bottom);
-	}
+.tabbar-box {
+  height: 224rpx;
+  box-sizing: content-box;
+  margin-bottom: env(safe-area-inset-bottom);
+}
 
-	::v-deep {
-		.u-tabbar-item__icon {
-			margin-bottom: 6rpx;
-		}
-	}
+.tabbar-text {
+  font-size: 36rpx;
+  font-weight: 500;
+}
 
-	.iconfont {
-		font-size: 36rpx;
-		color: rgb(125, 126, 128);
-	}
+.iconfont {
+  font-size: 36rpx;
+  color: rgb(125, 126, 128);
+}
 
-	.active {
-		color: #FF8C00;
-	}
-
-	::v-deep {
-		.u-tabbar-item__text {
-			font-size: 36rpx;
-			font-weight: 500;
-		}
-	}
+.active {
+  color: #ff8c00;
+}
 </style>
