7abe8c71fc4aa1091a8124fe476a58fc1dddc23a
luoluoluo <360646101@qq.com>
2025-11-06 09:33:21 +0800

    navbar 左侧菜单0。5px border

--- uni-running/components/navbar.vue
+++ uni-running/components/navbar.vue
@@ -208,8 +208,24 @@ onUnmounted(() => {
     background-color: #ffffff;
     border-radius: 999px;
     padding: 0 6px;
-    border: 1px solid rgba(0, 0, 0, 0.2);
     // box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      border: 1px solid rgba(0, 0, 0, 0.2);
+      border-radius: 999px;
+      transform: scale(0.5);
+      transform-origin: 0 0;
+      width: 200%;
+      height: 200%;
+      box-sizing: border-box;
+      pointer-events: none;
+    }
 
     .navbar-back-btn,
     .navbar-home-btn {
