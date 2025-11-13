9530e39943cac5bf8f282543fb4f8f8b70d76982
luoluoluo <360646101@qq.com>
2025-11-13 11:07:48 +0800

    增加小程序更新检测

--- uni-running/App.vue
+++ uni-running/App.vue
@@ -3,7 +3,48 @@ import { onLaunch, onShow, onHide, onPageScroll } from "@dcloudio/uni-app";
 
 onLaunch(() => {
   console.log("App Launch");
+  // 检测小程序更新
+  checkUpdate();
 });
+
+// 检测小程序更新
+const checkUpdate = () => {
+  // #ifdef MP-WEIXIN
+  if (uni.getUpdateManager) {
+    const updateManager = uni.getUpdateManager();
+
+    // 检查更新
+    updateManager.onCheckForUpdate((res) => {
+      if (res.hasUpdate) {
+        console.log("发现新版本，准备下载");
+      }
+    });
+
+    // 下载更新
+    updateManager.onUpdateReady(() => {
+      uni.showModal({
+        title: "更新提示",
+        content: "新版本已经准备好，是否重启应用？",
+        success: (res) => {
+          if (res.confirm) {
+            // 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        },
+      });
+    });
+
+    // 更新失败
+    updateManager.onUpdateFailed(() => {
+      uni.showModal({
+        title: "更新失败",
+        content: "新版本下载失败，请删除小程序后重新搜索打开",
+        showCancel: false,
+      });
+    });
+  }
+  // #endif
+};
 onShow(() => {
   console.log("App Show");
 });
