e4feb203dcef92c29d9613109e382d9d11334d01
Siming <xiaojieO_O@126.com>
2026-01-08 21:35:08 +0800

    enhance: 加载图片添加 loading 图，下载按钮挪到右下角

--- uni-running/pagesDashboard/eventAlbum/components/PreviewMedia.vue
+++ uni-running/pagesDashboard/eventAlbum/components/PreviewMedia.vue
@@ -3,17 +3,6 @@
 	<up-popup :show="isShowModal" @close="close" overlayOpacity="0.9" bgColor="transparent" mode="center" closeable>
 		<view class="flex-center" style="height: 100vh;width:100vw">
 			<SwiperSection ref="refSwiper" :originIndex="originIndex" @loadingMore="loadingMore" v-if="isShowModal" />
-			<view class="flex-center" style="position: fixed;left:0;bottom: 90rpx;width: 100%;">
-				<!-- <view class="slider-range" style="width: 686rpx;">
-					<view>测试</view>
-					<SliderRange v-model="sliderRange" solo :decoration="false" :max="60" />
-				</view> -->
-
-				<up-button @click="downloadPicture" type="primary" shape="circle" icon="download"
-					customStyle="width:186rpx;height:60rpx;">
-					下载原图
-				</up-button>
-			</view>
 		</view>
 	</up-popup>
 	</view>
@@ -57,48 +46,6 @@ import SwiperSection from "./rswiper.vue"
 		isShowModal.value = false;
 	}
 
-	function downloadPicture() {
-		const currentIndex = refSwiper.value?.originIndex;
-		const list = refSwiper.value?.originList;
-		const imageUrl = list?.[currentIndex];
-
-		if (!imageUrl) {
-			uni.showToast({ title: '获取图片失败', icon: 'none' });
-			return;
-		}
-
-		uni.showLoading({ title: '下载中...' });
-
-		uni.downloadFile({
-			url: imageUrl,
-			success: (res) => {
-				if (res.statusCode === 200) {
-					uni.saveImageToPhotosAlbum({
-						filePath: res.tempFilePath,
-						success: () => {
-							uni.hideLoading();
-							uni.showToast({ title: '保存成功', icon: 'success' });
-						},
-						fail: (err) => {
-							uni.hideLoading();
-							if (err.errMsg?.includes('auth deny')) {
-								uni.showToast({ title: '请授权相册权限', icon: 'none' });
-							} else {
-								uni.showToast({ title: '保存失败', icon: 'none' });
-							}
-						}
-					});
-				} else {
-					uni.hideLoading();
-					uni.showToast({ title: '下载失败', icon: 'none' });
-				}
-			},
-			fail: () => {
-				uni.hideLoading();
-				uni.showToast({ title: '下载失败', icon: 'none' });
-			}
-		});
-	}
 	defineExpose({
 		openModal,
 		close,
