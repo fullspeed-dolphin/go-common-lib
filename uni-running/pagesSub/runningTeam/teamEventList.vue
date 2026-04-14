cc4e5d36ad0ade89cc656d620b6ac94a45d9da0e
SimingChen <Siming123@@>
2026-04-14 14:57:11 +0800

    ui:注释更新按钮

--- uni-running/pagesSub/runningTeam/teamEventList.vue
+++ uni-running/pagesSub/runningTeam/teamEventList.vue
@@ -57,9 +57,9 @@
             </view>
             <!-- Action Buttons -->
             <view class="card-actions">
-              <u-button v-if="!['EXP', 'ACT'].includes(item.status)" type="primary" size="mini" shape="circle" color="#2979ff"
+              <!-- <u-button v-if="!['EXP', 'ACT'].includes(item.status)" type="primary" size="mini" shape="circle" color="#2979ff"
                 customStyle="margin:0;width:120rpx;height:52rpx;font-size:22rpx;"
-                @click.stop="editEvent(item)">更新</u-button>
+                @click.stop="editEvent(item)">更新</u-button> -->
               <u-button type="error" size="mini" shape="circle" color="#f56c6c"
                 customStyle="margin:0;width:120rpx;height:52rpx;font-size:22rpx;"
                 @click.stop="removeItem(item)">删除</u-button>
