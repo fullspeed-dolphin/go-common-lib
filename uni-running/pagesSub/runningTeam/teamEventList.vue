df04a65617cd95260af638b740c7f4548afd39ca
SimingChen <Siming123@@>
2026-04-23 20:00:22 +0800

    ui:表单都为必填

--- uni-running/pagesSub/runningTeam/teamEventList.vue
+++ uni-running/pagesSub/runningTeam/teamEventList.vue
@@ -60,17 +60,19 @@
               <!-- <u-button v-if="!['EXP', 'ACT'].includes(item.status)" type="primary" size="mini" shape="circle" color="#2979ff"
                 customStyle="margin:0;width:120rpx;height:52rpx;font-size:22rpx;"
                 @click.stop="editEvent(item)">更新</u-button> -->
-                 <u-button v-if="item.is_free == 1" type="primary" size="mini" shape="circle" color="#2979ff"
-                customStyle="margin:0;width:120rpx;height:52rpx;font-size:22rpx;"
-                @click.stop="editEvent(item)">更新</u-button>
-              <u-button type="error" size="mini" shape="circle" color="#f56c6c"
-                customStyle="margin:0;width:120rpx;height:52rpx;font-size:22rpx;"
+                 <u-button v-if="item.is_free == 1" type="primary" shape="circle" color="#2979ff"
+                customStyle="margin:0;width:100rpx;height:52rpx;font-size:22rpx;"
+                @click.stop="editEvent(item)">
+                    更新
+              </u-button>
+              <u-button type="error"  shape="circle" color="#f56c6c"
+                customStyle="margin:0;width:100rpx;height:52rpx;font-size:22rpx;"
                 @click.stop="removeItem(item)">删除</u-button>
-              <u-button v-if="detail.user_role === 'creator'" type="primary" size="mini" plain shape="circle" color="#10B981"
+              <u-button v-if="detail.user_role === 'creator'" type="primary"  plain shape="circle" color="#10B981"
                 customStyle="margin:0;width:160rpx;height:52rpx;font-size:22rpx;"
                 @click.stop="viewRegistrations(item)">报名信息</u-button>
-              <u-button type="primary" size="mini" plain shape="circle" color="#FF8C00"
-                customStyle="margin:0;width:120rpx;height:52rpx;font-size:22rpx;"
+              <u-button type="primary"  plain shape="circle" color="#FF8C00"
+                customStyle="margin:0;width:100rpx;height:52rpx;font-size:22rpx;"
                 @click.stop="viewEvent(item)">详情</u-button>
             </view>
           </view>
@@ -403,6 +405,7 @@ onShow(() => {
   justify-content: flex-start;
   gap: 12rpx;
   margin-top: 4rpx;
+  flex-shrink: 0;
 }
 
 .card-chevron {
